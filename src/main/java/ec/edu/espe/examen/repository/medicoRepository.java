7621dafc60f8db62b4e82c5dbb40fd5330b7868f
Carlos Fernández Mantilla <cefernandez7@espe.edu.ec>
2026-02-02 21:24:24 -0500

    Entrega de examen

--- src/main/java/ec/edu/espe/examen/repository/medicoRepository.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.espe.examen.repository;
-
-import ec.edu.espe.examen.entities.medico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface medicoRepository extends JpaRepository<medico, UUID> {
-}
--- src/main/java/ec/edu/espe/examen/repository/pacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.espe.examen.repository;
-
-import ec.edu.espe.examen.entities.paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface pacienteRepository extends JpaRepository<paciente, UUID> {
-}
